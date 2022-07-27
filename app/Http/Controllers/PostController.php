602bed7c63a39877b672c60ff1b5d06e8539665a
Genmai <hotaru103bsb@gmail.com>
2022-07-27 21:26:17 +0900

    docコメントを追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * (get)フォームを表示
+     * 
+     * @return View
+     */
     public function showCreatePage()
     {
         return view('posts.create');
